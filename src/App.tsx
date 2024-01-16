1e1884ab9e4a01cbee6ca155f489d3de617a513e
Omar De Angelis <omar.hpvfilm@gmail.com>
2024-01-16 20:09:47 +0100

    project link

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { VideoPlayer } from "./components/video-player";
+
+function App() {
+  return (
+    <>
+      <VideoPlayer />
+    </>
+  );
+}
+
+export default App;
